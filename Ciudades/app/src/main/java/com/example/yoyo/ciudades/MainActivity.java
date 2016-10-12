e89756abd55b104e562062f33a3663467ca93c20
Sergi 10 <sergi.droid@gmail.com>
2016-10-12 07:39:37 +0100

    Añadir listener a los spinner

--- Ciudades/app/src/main/java/com/example/yoyo/ciudades/MainActivity.java
+++ Ciudades/app/src/main/java/com/example/yoyo/ciudades/MainActivity.java
@@ -9,17 +9,29 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private Spinner ciudades_Spinner;
-    private Spinner paises_Spinner;
-    private TextView nombreCiudad;
-    public ArrayAdapter<CharSequence> adapterCiudades;
-    public ArrayAdapter<CharSequence> adapterPaises;
+public class MainActivity extends AppCompatActivity  {
+    public Spinner ciudades_Spinner;
+    public Spinner paises_Spinner;
+    public TextView nombreCiudad;
+    public String paisSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Inicializar los componentes
+        paises_Spinner = (Spinner) findViewById(R.id.paises);
+        ciudades_Spinner = (Spinner) findViewById(R.id.ciudades);
+        nombreCiudad = (TextView) findViewById(R.id.texto);
+        initSpinner();
+
+
+
+
+
+
+
+       /* setContentView(R.layout.activity_main);
         nombreCiudad = (TextView) findViewById(R.id.texto);
         ciudades_Spinner = (Spinner) findViewById(R.id.ciudades);
         //ciudades_Spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
@@ -30,30 +42,60 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adapterPaises.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         paises_Spinner.setAdapter(adapterPaises);
         paises_Spinner.setOnItemSelectedListener(this);
-
+*/
     }
+    public class MyOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if (parent.getId() == R.id.paises) {
+                
+
+
+
+                paisSelected = (parent.getItemAtPosition(position).toString());
+                nombreCiudad.setText(paisSelected);
+            }
+            if (parent.getId() == R.id.ciudades) {
+                paisSelected = (parent.getItemAtPosition(position).toString());
+                nombreCiudad.setText(paisSelected);
+            }
+            //Podemos hacer varios ifs o un switchs por si tenemos varios spinners.
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+    private void initSpinner() {
+        // Creamos el adaptador
+        ArrayAdapter<CharSequence> adapterPaises = ArrayAdapter.createFromResource(this, R.array.paises, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ciudadesespana, android.R.layout.simple_spinner_item);
+        // Creamos el Listener
+        AdapterView.OnItemSelectedListener escuchador = new  MyOnItemSelectedListener();
+        // Elegimos el tipo grafico de spinner
+        adapterPaises.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_selectable_list_item);
+        //Le indicamos al spinner el adaptador a usar
+        paises_Spinner.setAdapter(adapterPaises);
+        ciudades_Spinner.setAdapter(adapter);
+        // Le añadimos el listener al spinner
+        paises_Spinner.setOnItemSelectedListener(escuchador);
+        ciudades_Spinner.setOnItemSelectedListener(escuchador);
+       // this.ciudades_Spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+    }
+    }
+
+
+
+/*
     public void onItemSelectedListener(){
-        if (paises_Spinner.getSelectedItem().toString().equals("España"))
-        {
-            adapterCiudades= ArrayAdapter.createFromResource(this, R.array.ciudadesespana, android.R.layout.simple_spinner_dropdown_item);
-        }
-        else if (paises_Spinner.getSelectedItem().toString().equals("Alemania"))
-        {
-            adapterCiudades= ArrayAdapter.createFromResource(this, R.array.ciudadesalemania, android.R.layout.simple_spinner_dropdown_item);
-        }
-        else if (paises_Spinner.getSelectedItem().toString().equals("Francia"))
-        {
-            adapterCiudades= ArrayAdapter.createFromResource(this, R.array.ciudadesfrancia, android.R.layout.simple_spinner_dropdown_item);
-        }
-        ciudades_Spinner.setAdapter(adapterCiudades);
-        ciudades_Spinner.setOnItemSelectedListener(this);
-    }
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
             nombreCiudad.setText(ciudades_Spinner.getSelectedItem().toString());
             //nombreCiudad.setText(parent.getSelectedItem().toString());
     }
     @Override
     public void onNothingSelected(AdapterView<?> arg0) {
     }
-}
+    */
+
